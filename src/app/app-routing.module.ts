1d08c26d97dab203fb6df4fcd84d2b34d67942a1
Ramya <ramya.bala221190@gmail.com>
2023-11-21 11:31:37 +0530

    added todos and comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
+import { CommentsComponent } from './comments/comments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'todos',
+    component:TodosComponent
+  },
+  {
+    path:'comments',
+    component:CommentsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
