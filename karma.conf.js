01d3c34fa27c11dd780de1e8aabd3558a17b861c
Ramya <ramya.bala221190@gmail.com>
2023-12-05 09:51:28 +0530

    updated

--- karma.conf.js
+++ karma.conf.js
@@ -7,20 +7,12 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-coverage-istanbul-reporter'),
       require('karma-coverage'),
-      require('karma-mocha-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['mocha','coverage'],
     coverageReporter: {
       type: 'text-summary'
     },
