2ef567d73816f435cfa09c128fd32b555f36cc8f
Ramya <ramya.bala221190@gmail.com>
2023-12-07 20:35:06 +0530

    updated

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
-const os = require('os');
-const chromeHeadlessSupported = os.platform() !== 'win32' || Number((os.release().match(/^(\d+)/) || ['0', '0'])[1]) >= 10;
+const path=require('path');
+
+process.env.CHROME_BIN=require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -25,8 +26,8 @@ module.exports = function (config) {
       'ChromeHeadless'
     ],
     customLaunchers: {
-      ChromeHeadless: {
-          base: 'Chrome',
+      ChromeHeadlessNoSandbox: {
+          base: 'ChromeHeadless',
           flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
       }
     },
