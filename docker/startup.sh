12b838c9229af8cafd8ff5e0ab1a4920240595af
Ramya <ramya.bala221190@gmail.com>
2023-12-09 22:15:33 +0530

    updated

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,5 @@
 echo "Starting application..."
 echo "name = ${name}"
 echo "port = ${port}"
-envsubst < "/usr/share/nginx/html/assets/runtime-environment.json"
+envsubst < "/usr/share/nginx/html/assets/runtime-environment.json" | sponge "/usr/share/nginx/html/assets/runtime-environment.json"
 nginx -g 'daemon off;'
