0566b102939254fd596a17016d63a343d61985d5
Ramya <ramya.bala221190@gmail.com>
2023-12-12 11:47:58 +0530

    update

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,5 +2,5 @@
 echo "Starting Container..."
 echo "name = ${name}"
 echo "port = ${port}"
-envsubst '$env,$port' < "/usr/share/nginx/html/assets/runtime-environment.js" > "/usr/share/nginx/html/assets/runtime-environment.js"
+envsub "/usr/share/nginx/html/assets/runtime-environment.js"
 nginx -g 'daemon off;'
