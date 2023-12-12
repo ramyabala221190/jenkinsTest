1bd66d18bd6023e36fd3cab62c13d75ac57c8530
Ramya <ramya.bala221190@gmail.com>
2023-12-12 20:56:56 +0530

    updated

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,7 +4,7 @@ echo "env = ${env}"
 echo "port = ${port}"
 replaceValue='dev'
 replaceWith="${env}"
-newStr=${"/usr/share/nginx/html/assets/runtime-environment.js"//$replaceValue/$replaceWith}
-echo $newStr
+str="/usr/share/nginx/html/assets/runtime-environment.js"
+newStr=${str//$replaceValue/$replaceWith}
+echo "${newStr}"
 nginx -g 'daemon off;'
-# envsub "/usr/share/nginx/html/assets/runtime-environment.js"
