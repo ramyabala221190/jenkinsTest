30b40f7c1a41a0bd0534307a89f2d4b3978e3d92
Ramya <ramya.bala221190@gmail.com>
2023-12-12 11:36:02 +0530

    updated

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,13 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 echo "Starting Container..."
 echo "name = ${name}"
 echo "port = ${port}"
-
-echo "$1"
-# envsubst < "/usr/share/nginx/html/assets/runtime-environment.json" | sponge "/usr/share/nginx/html/assets/runtime-environment.json"
-#envsubst '$name,$port' < "/usr/share/nginx/html/assets/runtime-environment.js"
-#cat "/usr/share/nginx/html/assets/runtime-environment.js"
-# tmpfile=$(mktemp)
-# cp "/usr/share/nginx/html/assets/runtime-environment.json" $tmpfile
-# envsubst '$name,$port' < $tmpfile  && mv $tmpfile "/usr/share/nginx/html/assets/runtime-environment.json"
+envsubst '$env,$port' < "/usr/share/nginx/html/assets/runtime-environment.js" > "/usr/share/nginx/html/assets/runtime-environment.js"
 nginx -g 'daemon off;'
