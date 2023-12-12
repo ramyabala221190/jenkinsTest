e88db0308ef2fcc857a78402161e609f49a30bd4
Ramya <ramya.bala221190@gmail.com>
2023-12-12 21:07:03 +0530

    updated

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,8 +2,8 @@
 echo "Starting Container..."
 echo "env = ${env}"
 echo "port = ${port}"
-replaceValue='DEV'
-replaceWith="${env}"
+replaceValue=''
+replaceWith="prod"
 str="$(< /usr/share/nginx/html/assets/runtime-environment.js)"
 newStr=${str//$replaceValue/$replaceWith}
 echo "${newStr}"
