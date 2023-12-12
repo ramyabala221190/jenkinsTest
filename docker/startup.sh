752399615cad8cbbaa5d5c7d95477d8ef89e551e
Ramya <ramya.bala221190@gmail.com>
2023-12-12 15:37:49 +0530

    updated

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,14 +2,6 @@
 echo "Starting Container..."
 echo "env = ${env}"
 echo "port = ${port}"
-
-args_array=("$@")
-for i in "${args_array[@]}"
-do
-  :
-  echo "### Got variable $i ###"
-done
-echo "args_count = $#"
-
+echo "args_count = $@"
 # envsub "/usr/share/nginx/html/assets/runtime-environment.js"
 nginx -g 'daemon off;'
