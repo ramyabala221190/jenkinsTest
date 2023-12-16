967238c4215f8297c6816d84610888021f45ede4
Ramya <ramya.bala221190@gmail.com>
2023-12-16 20:51:55 +0530

    updated

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,4 +2,5 @@
 echo "Starting Container..."
 echo "Runtime arguments: env = ${env}"
 echo "Runtime arguments: port = ${port}"
+cp /usr/share/nginx/html/assets/${env}.config.json /usr/share/nginx/html/assets/
 nginx -g 'daemon off;'
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+import { environmentLoader } from 'envLoader';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
