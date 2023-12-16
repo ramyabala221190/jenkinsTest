ed52facd0008dcab220286b8697aba2641fd64a8
Ramya <ramya.bala221190@gmail.com>
2023-12-16 22:10:20 +0530

    updated

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Starting Container..."
 echo "Runtime arguments: env = ${env}"
-rm -f /usr/share/nginx/html/assets/config.json
 cp -f /usr/share/nginx/html/assets/environments/${env}/config.json /usr/share/nginx/html/assets/config.json
 nginx -g 'daemon off;'
