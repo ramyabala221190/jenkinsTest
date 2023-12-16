09e0aefda0fdfcae457b2496f147099e5db4660d
Ramya <ramya.bala221190@gmail.com>
2023-12-16 22:34:32 +0530

    updated

--- src/app/environment-config.service.ts
+++ src/app/environment-config.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
-import { map, tap } from 'rxjs';
-import { HammerGestureConfig } from '@angular/platform-browser';
+import { tap } from 'rxjs';
 
 
 export interface environmentConfig{
