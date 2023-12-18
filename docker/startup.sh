e2d58ee553546f26d43278a64ae587db887270de
Ramya <ramya.bala221190@gmail.com>
2023-12-18 15:52:33 +0530

    updated

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp /usr/share/nginx/html/assets/environments/${env}/config.json /usr/share/nginx/html/assets/runtime-environment.json
+mv /usr/share/nginx/html/assets/environments/${env}/config.json /usr/share/nginx/html/assets/runtime-environment.json
 nginx -g 'daemon off;'
