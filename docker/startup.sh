669a7208890be7e01b42b2aef314e7d849408c78
Ramya <ramya.bala221190@gmail.com>
2023-12-18 18:52:44 +0530

    added uat env

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "Starting container. Runtime env variables: env=${env}"
 cp /usr/share/nginx/html/assets/environments/${env}.config.json /usr/share/nginx/html/assets/environments/runtime-environment.json
 nginx -g 'daemon off;'
